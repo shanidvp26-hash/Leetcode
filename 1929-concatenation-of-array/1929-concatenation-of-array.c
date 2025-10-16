bb00340e38e05f7e5e2075ed1169520167b9fcd6
shanidvp26-hash <shanidvp26@gmail.com>
2025-10-16 15:05:50 +0530

    Time: 0 ms (100%), Space: 14.4 MB (25.94%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    int* ans = (int*)malloc(sizeof(int) * numsSize * 2);
+    for (int i = 0; i < numsSize; i++) {
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+    *returnSize = numsSize * 2;
+    return ans;
+}
