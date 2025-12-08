044ded409797e1259c62e5a8d35613d2a9b05d97
shanidvp26-hash <shanidvp26@gmail.com>
2025-12-08 14:08:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (40.02%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.c
@@ -0,0 +1,12 @@
+int arraySign(int* nums, int numsSize) {
+    int sign = 1;
+
+    for(int i = 0; i < numsSize; i++){
+        if(nums[i] == 0)
+            return 0;        // Product becomes zero
+        else if(nums[i] < 0)
+            sign = -sign;    // Flip sign for each negative number
+    }
+
+    return sign;
+}
