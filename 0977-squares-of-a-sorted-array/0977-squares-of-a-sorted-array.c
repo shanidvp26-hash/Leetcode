e6e41fca640e33cb9bf678bc69940c2ee7a4c0b6
shanidvp26-hash <shanidvp26@gmail.com>
2025-12-23 09:24:55 +0530

    Time: 3 ms (40.43%), Space: 24.6 MB (77.19%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    int left = 0, right = numsSize - 1;
+    int pos = numsSize - 1;
+
+    while (left <= right) {
+        int leftSq = nums[left] * nums[left];
+        int rightSq = nums[right] * nums[right];
+
+        if (leftSq > rightSq) {
+            result[pos--] = leftSq;
+            left++;
+        } else {
+            result[pos--] = rightSq;
+            right--;
+        }
+    }
+
+    *returnSize = numsSize;
+    return result;
+}
